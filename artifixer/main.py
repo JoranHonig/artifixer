e83f6cb6c94592d060afec9cf203554884f7bd44
Joran Honig <joran.honig@gmail.com>
2018-12-27 21:39:58 +0100

    cleanup

--- artifixer/main.py
+++ artifixer/main.py
@@ -46,11 +46,11 @@ def cli():
 
 @cli.command()
 @click.option('-e', help="Strength of the filter", default=1.6)
-@click.option('-b', help="blur radius", default=20)
-@click.option('-t', help="blur threshold", default = 10)
+@click.option('-b', help="Blur radius", default=20)
+@click.option('-t', help="Blur threshold.", default=10)
 @click.option('--input', help="Input image file", required=True)
-@click.option('--base', help="Base image to build profile off", required=True)
-@click.option('--output', help="Output image file", required=True)
+@click.option('--base', help="Base even image, this is used to find the dust specs and calculate the filter", required=True)
+@click.option('--output', help="Name of the output image", required=True)
 def newton(e, b, t, input, base, output):
     img = Image.open(base)
     to_fix = Image.open(input)
@@ -60,14 +60,11 @@ def newton(e, b, t, input, base, output):
     e_img = img.filter(BoxBlur(70))
     profile = divide(img, e_img)
 
-    fixed = to_fix.copy()
-    p_fixed = fix(to_fix, img, profile, e)
-    p_fixed.save(output, "PNG")
+    fixed = fix(to_fix, img, profile, e)
+    fixed.save(output, "PNG")
 
     R, G, B = 0, 1, 2
-
     i_split = img.split()
-
     def rf(p):
         if abs(p - avg[R]) > t:
             return 255
@@ -86,19 +83,15 @@ def newton(e, b, t, input, base, output):
         else:
             return 0
     mr = i_split[R].point(rf)
-
     mg = i_split[G].point(gf)
-
     mb = i_split[B].point(bf)
-
-    fixed.paste(p_fixed, None, None)
 
     blurred = fixed.filter(BoxBlur(b))
     fixed.paste(blurred, None, mr)
     fixed.paste(blurred, None, mg)
     fixed.paste(blurred, None, mb)
 
-    fixed.save("test_" + output, "PNG")
+    fixed.save("blurred_" + output, "PNG")
 
 
 if __name__ == "__main__":
